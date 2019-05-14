d501c3b4083452422bd78d7eb52705d7a162237b
gianpy15 <gpdipietro@gmail.com>
2019-05-14 16:26:57 +0200

    update dockerfile for pfa

--- pfa/run.sh
+++ pfa/run.sh
@@ -1,6 +1,8 @@
-rm -rf /root/workspace/PhysicsForAffordances/build/*
-cd /root/workspace/PhysicsForAffordances/build
+#!/bin/bash
+
+rm -rf /exp/workspace/PhysicsForAffordances/build/*
+cd /exp/workspace/PhysicsForAffordances/build
 cmake ..
-make DataCreator -j4
+make DataCreator -j10
 Xvfb :19 -screen 0 1024x768x16 &
-/root/Libraries/conda/envs/pfa/bin/python3.7 /root/workspace/PhysicsForAffordances/python/source/data_creation/tasker.py $1 --logstdout --logstderr
+/exp/Libraries/conda/envs/pfa/bin/python3.7 /exp/workspace/PhysicsForAffordances/python/source/data_creation/tasker.py $1 --logstdout --logstderr
